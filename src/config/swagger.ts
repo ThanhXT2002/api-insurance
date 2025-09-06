e5dd69deac80b0d18bfd06b1f73cd03aff1d6bac
ThanhXT2002 <tranxuanthanhtxt2002@gmail.com>
2025-09-07 01:15:30 +0700

    fix: add Swagger UI documentation endpoint and serve swagger.json

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -61,7 +61,53 @@ const options: swaggerJsdoc.Options = {
 const swaggerSpec = swaggerJsdoc(options)
 
 export function setupSwagger(app: Express) {
-	app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+	// Serve swagger.json
+	app.get('/swagger.json', (_req, res) => {
+		res.setHeader('Content-Type', 'application/json')
+		res.send(swaggerSpec)
+	})
+	
+	// Custom Swagger UI HTML để tránh vấn đề với static files
+	app.get('/docs', (_req, res) => {
+		const html = `
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<title>API Insurance Documentation</title>
+			<link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@4.15.5/swagger-ui.css" />
+			<style>
+				html { box-sizing: border-box; overflow: -moz-scrollbars-vertical; overflow-y: scroll; }
+				*, *:before, *:after { box-sizing: inherit; }
+				body { margin:0; background: #fafafa; }
+			</style>
+		</head>
+		<body>
+			<div id="swagger-ui"></div>
+			<script src="https://unpkg.com/swagger-ui-dist@4.15.5/swagger-ui-bundle.js"></script>
+			<script src="https://unpkg.com/swagger-ui-dist@4.15.5/swagger-ui-standalone-preset.js"></script>
+			<script>
+				window.onload = function() {
+					const ui = SwaggerUIBundle({
+						url: '/swagger.json',
+						dom_id: '#swagger-ui',
+						deepLinking: true,
+						presets: [
+							SwaggerUIBundle.presets.apis,
+							SwaggerUIStandalonePreset
+						],
+						plugins: [
+							SwaggerUIBundle.plugins.DownloadUrl
+						],
+						layout: "StandaloneLayout"
+					});
+				};
+			</script>
+		</body>
+		</html>
+		`
+		res.send(html)
+	})
 }
 
 export default swaggerSpec
